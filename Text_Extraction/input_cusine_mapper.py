049553fcc6be316834fca0aa17ce41cef6fe1c88
Abu  Abraham <u6325322@anu.edu.au>
2017-09-09 04:37:23 +1000

    Text extraction updates, NativeScript not added

--- Text_Extraction/input_cusine_mapper.py
+++ Text_Extraction/input_cusine_mapper.py
@@ -1,10 +1,36 @@
 from __future__ import division
+from fuzzywuzzy import fuzz
+import enchant
+
+
+def dish_analyzer(input_text):
+    input_text = input_text.split()
+    result = []
+    d = enchant.Dict("en_US")
+    for word in input_text:
+        if not d.check(str(word)):
+            result.append(word);
+            input_text = ""
+    for word in result:
+        input_text = input_text+word+" ";
+    return input_text
+
+def remove_decorative_words(food_item):
+    decorative_list = ['yummy','amazing','tasty','beautiful','delightful','exuberant','healthy','magnificent','plain','rich']
+    return_word = ""
+    for word in food_item.split():
+        if word not in decorative_list:
+            return_word = return_word+word
+    return return_word
+
+
+
 with open('cusine_list') as f:
     content = f.readlines();
 f.close()
-
-food_item = "Grilled Chicken"
+food_item = "Yummy Butter Chicken"
 food_item = food_item.lower()
+food_item = remove_decorative_words(food_item)
 max = 0;
 cluster = "";
 for cuisine in content:
@@ -13,7 +39,7 @@ for cuisine in content:
         content = f.readlines();
         content = list(content);
         for item in content:
-            if food_item in str(item).lower():
+            if  fuzz.token_set_ratio(dish_analyzer(food_item),str(item).lower()) > 90:
                 c+=1;
         if (c/len(content)) > max:
             cluster = cuisine
@@ -24,3 +50,4 @@ print cluster
 
 
 ##TODO : IMPROVE INCORPORATING DOC_ANALYSIS LESSONS
+##TODO : MAP EACH ITEM TO NEAREST ONE AND THEN MAP STUFF LIKE BUTTER CHICKN TO MURG MAKHANI -- BING SEARCH
